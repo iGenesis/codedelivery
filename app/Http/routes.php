d75b64e658cd84ee1902a8f162a9118cc5dfc669
iGenesis Digital <lucianothome@igenesisdigital.com>
2016-03-25 19:18:16 -0300

    PrimeiroCommit
    Commit para testar

---
+++ app/Http/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::get('admin/categories', 'CategoriesController@index');
+    Route::get('admin/categories/create', ['as'=>'admin.categories.create', 'uses'=>'CategoriesController@create']);
+
+});
